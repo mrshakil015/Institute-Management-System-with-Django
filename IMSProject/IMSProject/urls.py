de87fe1ce286ed7d145a7f6f947aa3bdd759f1cd
Makishima21 <89250014+Makishima21@users.noreply.github.com>
2024-06-24 22:31:39 -0600

    courseviews and urls and extends

--- IMSProject/IMSProject/urls.py
+++ IMSProject/IMSProject/urls.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django.urls import path
 from IMSapp.views import *
+from IMSapp.courseviews import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',homepage,name="homepage"),
     path('contactpage/',contactpage,name="contactpage"),
     path('dashboard/',dashboard,name="dashboard"),
+    
+    ##courses
+    
+    path('addcourse/',addcourse,name="addcourse"),
+    path('courselist/',courselist,name="courselist"),
+    path('editcourse/',editcourse,name="editcourse"),
+    path('viewcourse/',viewcourse,name="viewcourse"),
+    
+    
+    
+    
+    
 ]
---
+++ IMSProject/IMSapp/courseviews.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,redirect
+
+##courses
+def addcourse(request):
+    return render(request,'courses/addcourse.html.html')
+def courselist(request):
+    return render(request,'courses/courselist.html')
+def editcourse(request):
+    return render(request,'courses/editcourse.html')
+def viewcourse(request):
+    return render(request,'courses/viewcourse.html')
+
